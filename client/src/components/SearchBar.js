864d7557d2f6a553b80c7f7fc6dda3b67396f21d
ghermidont <mihai_olaru@ymail.com>
2022-02-24 16:17:20 +0200

    Implemented form validation.

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import {  TextField, IconButton } from "@material-ui/core";
 import { SearchOutlined } from "@material-ui/icons";
 import { useThemeContext } from "../context/themeContext";
 
 function SearchBar({query, setQuery}) {
     const { theme } = useThemeContext();
+    const [error, setError] = useState("");
+    const [label, setLabel] = useState("Search");
+
+const validator = (e) => {
+    //Regex for validating the input
+    if (e.target.value.match("^[a-zA-Z0-9_.-]*$")) {
+        setError("");
+        setLabel("Search");
+        setQuery(e.target.value);
+    } else {
+        setError("Invalid input.");
+        setLabel("Error");
+    }
+};
 
     return (
         <TextField
+            error={error.length!==0}
             id={`search-bar-${theme}`}
             variant="outlined"
-            placeholder="Search.."
+            placeholder="use only letters and numbers"
+            label={label}
             value={query}
-            onChange={(e) => setQuery(e.target.value)}
+            helperText={error}
+            onChange={(e) => validator(e)}
             InputProps={{
                 endAdornment: (
                     <IconButton>
